9b4597b88eef7ecdc622fc0f1ec53948617e7f30
Karmjeet Kaur <karmjeet.k@adorebeauty.com.au>
2022-09-08 15:52:52 +1000

    Added Drawer Navigation to app.jax

--- App.jsx
+++ App.jsx
@@ -4,22 +4,34 @@ import React from 'react';
 import { StyleSheet, Text, View  } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from './Screens/HomeScreen';
 import NewTweet from './Screens/NewTweet';
 import ProfileScreen from './Screens/ProfileScreen';
 import TweetScreen from './Screens/TweetScreen';
+import SettingsScreen from './Screens/SettingsScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-export default function App() {
- return (
-    <NavigationContainer>
-    <Stack.Navigator>
+const HomeStackNavigator= () =>{
+ return(
+       <Stack.Navigator>
             <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
             <Stack.Screen name="New Tweet" component={NewTweet}></Stack.Screen>
             <Stack.Screen name="Tweet Screen" component={TweetScreen}></Stack.Screen>
             <Stack.Screen name="Profile Screen" component={ProfileScreen}></Stack.Screen>
         </Stack.Navigator>
+ )
+}
+
+export default function App() {
+ return (
+    <NavigationContainer>
+     <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={HomeStackNavigator} />
+      <Drawer.Screen name="Settings" component={SettingsScreen} />
+    </Drawer.Navigator>
    </NavigationContainer>
  );
  } 
