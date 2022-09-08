b7fa3dc27c3ae4600ff833b0088efec4e6e51c0f
Karmjeet Kaur <karmjeet.k@adorebeauty.com.au>
2022-09-08 16:24:42 +1000

    Added Bottom tab and Drawer navigation

--- App.jsx
+++ App.jsx
@@ -5,25 +5,41 @@ import { StyleSheet, Text, View  } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import {createDrawerNavigator} from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './Screens/HomeScreen';
 import NewTweet from './Screens/NewTweet';
 import ProfileScreen from './Screens/ProfileScreen';
 import TweetScreen from './Screens/TweetScreen';
 import SettingsScreen from './Screens/SettingsScreen';
+import SearchScreen from './Screens/SearchScreen';
+import NotificationsScreen from './Screens/NotificationsScreen';
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();  
+
 
 const HomeStackNavigator= () =>{
  return(
        <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+            <Stack.Screen name="Tab" component={TabNavgator}></Stack.Screen>
             <Stack.Screen name="New Tweet" component={NewTweet}></Stack.Screen>
             <Stack.Screen name="Tweet Screen" component={TweetScreen}></Stack.Screen>
             <Stack.Screen name="Profile Screen" component={ProfileScreen}></Stack.Screen>
         </Stack.Navigator>
  )
 }
+
+const TabNavgator= () =>{
+ return(
+       <Tab.Navigator>
+            <Tab.Screen name="Home1" component={HomeScreen} />
+            <Tab.Screen name="Search" component={SearchScreen} />
+            <Tab.Screen name="Notifications" component={NotificationsScreen} />
+        </Tab.Navigator>
+ )
+}
+
 
 export default function App() {
  return (
